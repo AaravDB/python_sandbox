6591f41bb40f083fe60b8b38634b01154010975a
AaravDB <b23cm1002@iitj.ac.in>
2025-05-06 15:06:51 +0530

    this is another commit from vs code in python and im just playing around

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,13 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name="aarav"
+age=20
+mental_age=5
 
+print(name)
+print(age)
+print(mental_age)
+print(f"hello,my name is {name} and I am {age} years old and my mental age is {mental_age}")
 # String Formatting
 
 # String Methods
