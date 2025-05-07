13c3099268cca3c269a0a8e2843140027ccb7d59
AaravDB <b23cm1002@iitj.ac.in>
2025-05-07 17:46:02 +0530

    ayo file handling to the rescue honey

--- python_sandbox_starter/files.py
+++ python_sandbox_starter/files.py
@@ -1,1 +1,15 @@
 # Python has functions for creating, reading, updating, and deleting files.
+myFile=open('myfile.txt','w')
+print(myFile.name)
+print(myFile.closed)
+print(myFile.mode)
+myFile.write('I love pasta')
+myFile.close()
+
+myFile=open('myfile.txt','a')
+myFile.write(' and I am a great person')
+myFile.close()
+myFile=open('myfile.txt','r+')
+text=myFile.read(100)
+print(text)
+
