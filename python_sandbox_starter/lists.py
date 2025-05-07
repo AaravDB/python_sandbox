2075786f9b490f0db7dcfb04d846413386800847
AaravDB <b23cm1002@iitj.ac.in>
2025-05-07 13:37:31 +0530

    another day another list of fruits to eat!

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,18 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+a=[1,2,3,4,5,6,7,8]
+print(a)
+
+fruits=['Apples','Oranges','Grapes','Pears']
+print(fruits[1])
+print(len(fruits))
+fruits.append('Mangos')
+print(fruits)
+fruits.insert(2,'bananas')
+print(fruits)
+fruits.pop(3)
+print(fruits)
+#len,insert,pop,sort,reverse
+fruits.reverse()
+print(fruits)
+fruits.sort(reverse=True)
+print(fruits)
