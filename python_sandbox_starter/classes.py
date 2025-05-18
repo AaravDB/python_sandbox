292e9ca462713d6fa971802eb23641c7d4b20670
AaravDB <b23cm1002@iitj.ac.in>
2025-05-18 16:57:46 +0530

    added code to classes

--- python_sandbox_starter/classes.py
+++ python_sandbox_starter/classes.py
@@ -1,2 +1,13 @@
 # A class is like a blueprint for creating objects. An object has properties and methods(functions) associated with it. Almost everything in Python is an object
 
+class user:
+    def __init__(self,name,age,hometown):
+        self.name=name
+        self.age=age
+        self.hometown=hometown
+    def greeting(self):
+        return f'Hello!My name is {self.name}, I am {self.age} years old and I belong to {self.hometown} .Where are you from?'
+
+brad=user('aarav',20,'chandigarh')
+print(brad.greeting())
+
